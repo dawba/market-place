f2f239c1ba34b875f07cbf851e44d8f51094f068
dawba <dawid.bania1@gmail.com>
2024-05-13 14:40:55 +0200

    Advertisement management C/S/R implementation

--- src/main/java/org/marketplace/controllers/AdvertisementManagementController.java
+++ src/main/java/org/marketplace/controllers/AdvertisementManagementController.java
@@ -7,6 +7,8 @@ import org.marketplace.services.AdvertisementManagementService;
 import org.marketplace.services.CategoryManagementService;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/advertisement")
 public class AdvertisementManagementController {
@@ -16,7 +18,47 @@ public class AdvertisementManagementController {
     }
 
     @PostMapping("/add")
-    public Advertisement requestAddOffer(@RequestBody Advertisement advertisement) {
+    public Advertisement requestAddAdvertisement(@RequestBody Advertisement advertisement) {
         return advertisementManagementService.addAdvertisement(advertisement);
     }
+
+    @DeleteMapping("/delete/{id}")
+    public void requestDeleteAdvertisement(@PathVariable Long id) {
+        advertisementManagementService.deleteAdvertisement(id);
+    }
+
+    @PutMapping("/update/{id}")
+    public Advertisement requestUpdateAdvertisement(@RequestBody Advertisement advertisement) {
+        return advertisementManagementService.updateAdvertisement(advertisement);
+    }
+
+    @GetMapping("/get/{id}")
+    public Advertisement requestGetAdvertisement(@PathVariable Long id) {
+        return advertisementManagementService.getAdvertisementById(id);
+    }
+
+    @GetMapping("get/all")
+    public List<Advertisement> requestGetAllAdvertisements() {
+        return advertisementManagementService.getAllAdvertisements();
+    }
+
+    @GetMapping("get/category/{id}")
+    public List<Advertisement> requestGetAdvertisementsByCategory(@PathVariable Long id) {
+        return advertisementManagementService.getAdvertisementsByCategory(id);
+    }
+
+    @GetMapping("get/user/{id}")
+    public List<Advertisement> requestGetAdvertisementsByUser(@PathVariable Long id) {
+        return advertisementManagementService.getAdvertisementsByUser(id);
+    }
+
+    /*
+
+    additional methods that can be implemented in the future:
+
+    - deleting all user ads
+    - deleting all category ads
+    - getting a sum of categories
+
+     */
 }
--- src/main/java/org/marketplace/repositories/AdvertisementManagementRepository.java
+++ src/main/java/org/marketplace/repositories/AdvertisementManagementRepository.java
@@ -4,7 +4,9 @@ import org.marketplace.models.Advertisement;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public interface AdvertisementManagementRepository extends JpaRepository<Advertisement, Long> {
-    List<Advertisement> findByCategoryId(Long categoryId);
+    List<Advertisement> findByCategory_Id(Long categoryId);
+    List<Advertisement> findByUser_Id(Long userId);
 }
