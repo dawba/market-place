573b9b018cb939297f4d9ebc96085540b2fd20d3
dpolishchuk <dpolishchuk@vmware.com>
2024-05-18 10:00:03 +0200

    adding auth module

--- src/main/java/org/marketplace/configuration/SecurityConfig.java
+++ src/main/java/org/marketplace/configuration/SecurityConfig.java
@@ -3,6 +3,7 @@ package org.marketplace.configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -50,4 +51,11 @@ public class SecurityConfig {
         return authenticationConfiguration.getAuthenticationManager();
     }
 
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("defaultUser")
+                .password(passwordEncoder().encode("defaultPass"))
+                .roles("USER");
+    }
 }
