24deec925294e77087c4a926e4295c21b47a695d
Konrad1w <komrad1214@gmail.com>
2024-05-28 18:03:50 +0200

    add user handling exception

--- src/main/java/org/marketplace/requests/GlobalExceptionHandler.java
+++ src/main/java/org/marketplace/requests/GlobalExceptionHandler.java
@@ -45,6 +45,12 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         return new ResponseEntity<>(errorMessage, HttpStatus.FORBIDDEN);
     }
 
+    @ExceptionHandler(DuplicateEntryException.class)
+    public ResponseEntity<ErrorResponse> handleDuplicateEntryException(DuplicateEntryException ex) {
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.CONFLICT.value(), ex.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(errorResponse);
+    }
+
     /*
 
      I could not find a solution to this problem - I cannot override the ResponseEntityExceptionHandler
